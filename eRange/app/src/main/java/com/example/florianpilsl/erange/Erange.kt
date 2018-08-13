c207ffe64b3cd16a23c64897eb09b04d96c5306f
florianpilsl <pilsl.96@web.de>
2018-08-13 18:51:57 +0200

    infacstrucutre added, we have to look up how to use fragments:x
    :x

---
+++ eRange/app/src/main/java/com/example/florianpilsl/erange/Erange.kt
@@ -0,0 +1,19 @@
+package com.example.florianpilsl.erange
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_erange.*
+
+class Erange : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_erange)
+        setSupportActionBar(toolbar)
+
+
+    }
+
+}
