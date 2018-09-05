0a0d23ea2760cde61611a599335d9f422341aaf5
AndreasHennings <daniel.schmidl@stud.uni-regensburg.de>
2018-09-05 22:30:22 +0200

    added basic fragment implementation

--- eRange/app/src/main/java/com/example/florianpilsl/erange/Erange.kt
+++ eRange/app/src/main/java/com/example/florianpilsl/erange/Erange.kt
@@ -1,19 +1,46 @@
 package com.example.florianpilsl.erange
 
+import android.app.FragmentManager
+import android.app.FragmentTransaction
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_erange.*
+import java.lang.reflect.Parameter
 
 class Erange : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_erange)
-        setSupportActionBar(toolbar)
+        supportFragmentManager.beginTransaction().replace(R.id.eRange, MapFragment.newInstance(), "MapFragment").commit()
+
+
+
+        goToMapAct.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.eRange, ParameterFragment.newInstance(), "ParameterFragment").commit()
+
+
+
+            //supportFragmentManager
+              //      .beginTransaction()
+                //    .add(R.id.eRange, MapFragment.newInstance(), "MapFragment")
+                  //  .commit()
+
+
+            Log.d("fragments" , "" +  supportFragmentManager.fragments.size)
+
+
+        }
+
 
 
     }
+
 
 }
--- eRange/app/src/main/java/com/example/florianpilsl/erange/MapFragment.kt
+++ eRange/app/src/main/java/com/example/florianpilsl/erange/MapFragment.kt
@@ -11,8 +11,22 @@ import android.view.ViewGroup
  */
 class MapFragment : Fragment() {
 
+
+    companion object {
+        fun newInstance(): MapFragment {
+
+            return MapFragment()
+
+        }
+    }
+
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_erange, container, false)
+        return inflater.inflate(R.layout.fragment_map, container, false)
+
     }
+
+
 }
